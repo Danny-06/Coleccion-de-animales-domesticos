2a1479e430b7f45bf74fdefc291a7161040cb7e5
Daniel Fabián <daniel.elbelaizi.alu@iescampanillas.com>
2021-12-13 14:18:49 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 
-import { HttpClientModule } from '@angular/common/http';
+
 import { AnimalComponent } from './components/animal/animal.component';
 import { UserComponent } from './components/user/user.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+// Http provider
+import { HttpClientModule } from '@angular/common/http';
 
 // NgModel
 import { FormsModule } from '@angular/forms';
 
 // PrimeNG
 import { InputTextModule } from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {ButtonModule} from 'primeng/button';
-import {DropdownModule} from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 
 // Dependency animation for some PrimeNg components
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Library to fix shadow dom issue with some PrimeNg components
 // https://github.com/maitrungduc1410/primeng-shadowdom-directives
